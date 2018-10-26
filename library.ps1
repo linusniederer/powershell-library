fdd9203685b14dabb995ea3224f2a91ec5b2676b
Niederer Linus <linusniederer@intra.local>
2018-10-26 08:30:18 +0200

    Create new function: "get-vm-ipaddress" to get the ip address of a virtual machine

---
+++ library.ps1
@@ -0,0 +1,41 @@
+﻿#
+# Powershell Skript by LINUS
+# 
+# 26.10.2018
+#
+# Dies ist eine Library für bestimmte Powershell Funktionen welche
+# in weitere Skritps eingebunden werden kann.
+#
+# Die Library darf verwendet und abgeändert werden.
+#
+#
+
+#
+# --------------------------------------------------------
+# Abschnitt: HYPER-V MANAGEMENT
+# --------------------------------------------------------
+#
+
+# type --> IPv4 / IPv6
+function global:get-vm-ipaddress ( $vmname, $type ) {
+
+    # get IPv4 Address
+    if ( $type -eq "IPv4" ) {
+        $network = ( GEt-VM -VMName $vmname | Get-VMNetworkAdapter).IpAddresses | Select-String -List 1;
+        $ipaddress = $network[0]
+    } 
+
+    # get IPv6 Address
+    if ( $type -eq "IPv6" ) {
+        $network = ( GEt-VM -VMName $vmname | Get-VMNetworkAdapter).IpAddresses | Select-String -List 1;
+        $ipaddress = $network[1]
+    } 
+
+    # if Type empty -> IPv4
+    else {
+        $network = ( GEt-VM -VMName $vmname | Get-VMNetworkAdapter).IpAddresses | Select-String -List 1;
+        $ipaddress = $network[0]
+    }
+
+    return $ipaddress;
+}
