61fc3d4f02e51ca53d020d47a5a2dda1e4435794
linusniederer <linus.niederer@bluemail.ch>
2020-04-15 22:57:45 +0200

    Create class-documentation.psm1

---
+++ src/classes/class-documentation.psm1
@@ -0,0 +1,15 @@
+class Documentation {
+
+    [string] $docPath
+
+    Documentation ( $path, [array] $files, [array] $folders ) {
+        
+        $this.docPath = $path
+
+        foreach ( $file in $files ) {
+
+        }
+    }
+
+
+}
