84680fa5c78742d5e8abd2951e45e807c8b1beb3
linusniederer <linus.niederer@bluemail.ch>
2020-04-16 10:18:15 +0200

    Update class-documentation.psm1

--- src/classes/class-documentation.psm1
+++ src/classes/class-documentation.psm1
@@ -2,16 +2,38 @@
 class Documentation {
 
     [string] $docPath
+    [string] $docName = "Documentation"
+    [string] $scriptPath
 
     # DOC: Klasse dokumentieren 
-    Documentation ( $path, [array] $files, [array] $folders ) {
+    Documentation ( $docPath, $scriptPath ) {
         
-        $this.docPath = $path
+        $this.docPath = $docPath
+        $this.scriptPath = $scriptPath
 
-        foreach ( $file in $files ) {
-
-        }
+        $this.create()
     }
 
+    # create documentation
+    hidden create() {
+        
+        New-Item -Path $this.docPath -Name $this.docName -ItemType Directory -Force
 
+        # Scan directories
+        $directories = Get-ChildItem -Directory $this.scriptPath -Recurse
+
+        foreach ($directroy in $directories ) {
+
+            $dirPath = $directroy.FullName
+
+            $files = Get-ChildItem -File $dirPath -Verbose
+
+            foreach ( $file in $files ) {
+                
+            }
+        }
+
+    }
+
+    
 }
