082a3d15d972ee361ec6cff5459c4c95ee3d9b99
linusniederer <linus.niederer@bluemail.ch>
2020-09-25 10:49:06 +0200

    Create class-logging.psm1

---
+++ src/classes/class-logging.psm1
@@ -0,0 +1,24 @@
+class Logging {
+
+    [String] $path = "$PSScriptRoot\log\"
+
+    # constructor
+    Logging () {
+
+    }
+
+    <#
+     # Method to log a message
+     # 
+     # param $message Message to Log
+     #>
+    [void] log ($message) {
+
+
+
+
+
+
+        
+    }
+}
