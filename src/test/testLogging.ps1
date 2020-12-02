766329c6e44337161ab8d6bd8025cba37c9977b8
linusniederer <linus.niederer@bluemail.ch>
2020-12-02 17:05:01 +0100

    Update testLogging.ps1

--- src/test/testLogging.ps1
+++ src/test/testLogging.ps1
@@ -1,11 +1,6 @@
-$env:PSModulePath += ";C:\Users\linusniederer.OPACC\myCloud\Projekte\Aktuell\GitHub.com\powershell-library\src\main\modules"
 Import-Module Logging
-
 
 Write-Log "There is a Warning in the Matrix"
 Write-Log "There is an Error in the Matrix!" 
 Write-Log "There is everything okay in the Matrix"
 Write-Log "Matrix is successful!"
-
-
-Remove-Module Logging
